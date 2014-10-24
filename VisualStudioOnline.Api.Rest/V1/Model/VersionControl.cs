ad18f7cb105656d344be9575ce37963269e95a82
OlegM <oleg_mikhaylov@hotmail.com>
2014-10-23 19:31:06 -0700

    Adding missing files

---
+++ VisualStudioOnline.Api.Rest/V1/Model/VersionControl.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace VisualStudioOnline.Api.Rest.V1.Model
+{
+    [DebuggerDisplay("{Path}")]
+    public class Branch : BaseObject
+    {
+        [JsonProperty(PropertyName = "path")]
+        public string Path { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "owner")]
+        public UserIdentity Owner { get; set; }
+
+        [JsonProperty(PropertyName = "createdDate")]
+        public DateTime CreatedDate { get; set; }
+
+        [JsonProperty(PropertyName = "relatedBranches")]
+        public List<Branch> RelatedBranches { get; set; }
+
+        [JsonProperty(PropertyName = "children")]
+        public List<Branch> Children { get; set; }
+
+        [JsonProperty(PropertyName = "mappings")]
+        public List<object> Mappings { get; set; }
+
+        [JsonProperty(PropertyName = "isDeleted")]
+        public bool IsDeleted { get; set; }
+    }
+
+    public class LabelReference
+    {
+        [JsonProperty(PropertyName = "self")]
+        public ObjectReference Self { get; set; }
+
+        [JsonProperty(PropertyName = "items")]
+        public ObjectReference Items { get; set; }
+
+        [JsonProperty(PropertyName = "owner")]
+        public ObjectReference Owner { get; set; }
+    }
+
+    [DebuggerDisplay("{Name}")]
+    public class Label : ObjectWithId<int>
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "labelScope")]
+        public string LabelScope { get; set; }
+
+        [JsonProperty(PropertyName = "modifiedDate")]
+        public DateTime ModifiedDate { get; set; }
+
+        [JsonProperty(PropertyName = "owner")]
+        public UserIdentity Owner { get; set; }
+
+        [JsonProperty(PropertyName = "_links")]
+        public LabelReference References { get; set; }
+    }
+
+    [DebuggerDisplay("{Path}")]
+    public class VersionControlItem : BaseObject
+    {
+        [JsonProperty(PropertyName = "version")]
+        public int Version { get; set; }
+
+        [JsonProperty(PropertyName = "path")]
+        public string Path { get; set; }
+
+        [JsonProperty(PropertyName = "isFolder")]
+        public bool IsFolder { get; set; }
+    }
+}
---
+++ VisualStudioOnline.Api.Rest/V1/VersionControlRestClient.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using VisualStudioOnline.Api.Rest.V1.Model;
+
+namespace VisualStudioOnline.Api.Rest.V1
+{
+    /// <summary>
+    /// TFS Version Control REST API client
+    /// </summary>
+    public class VersionControlRestClient : RestClientV1
+    {
+        protected override string SubSystemName
+        {
+            get
+            {
+                return "tfvc";
+            }
+        }
+
+        public VersionControlRestClient(string accountName, NetworkCredential userCredential, string collectionName = DEFAULT_COLLECTION)
+            : base(string.Format(ACCOUNT_ROOT_URL, accountName, collectionName), new BasicAuthenticationFilter(userCredential))
+        {
+        }
+
+
+        /// <summary>
+        /// Get a list of root branches
+        /// </summary>
+        /// <param name="includeChildren"></param>
+        /// <param name="includeParent"></param>
+        /// <param name="includeDeleted"></param>
+        /// <returns></returns>
+        public async Task<JsonCollection<Branch>> GetRootBranches(bool? includeChildren = null, bool? includeDeleted = null)
+        {
+            string response = await GetResponse("branches", new Dictionary<string, object>() { { "includeChildren", includeChildren }, { "includeDeleted", includeDeleted } });
+            return JsonConvert.DeserializeObject<JsonCollection<Branch>>(response);
+        }
+
+        /// <summary>
+        /// Get a branch
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="includeChildren"></param>
+        /// <param name="includeParent"></param>
+        /// <param name="includeDeleted"></param>
+        /// <returns></returns>
+        public async Task<Branch> GetBranch(string path, bool? includeChildren = null, bool? includeParent = null, bool? includeDeleted = null)
+        {
+            string response = await GetResponse(string.Format("branches/{0}", path), 
+                new Dictionary<string, object>() { { "includeChildren", includeChildren }, { "includeParent", includeParent }, { "includeDeleted", includeDeleted } });
+            return JsonConvert.DeserializeObject<Branch>(response);
+        }
+
+        /// <summary>
+        /// Get list of labels
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="owner"></param>
+        /// <param name="itemLabelFilter"></param>
+        /// <param name="top"></param>
+        /// <param name="skip"></param>
+        /// <returns></returns>
+        public async Task<JsonCollection<Label>> GetLabels(string name = null, string owner = null, string itemLabelFilter = null, int? top = null, int? skip = null)
+        {
+            string response = await GetResponse("labels",
+                new Dictionary<string, object>() { { "name", name }, { "owner", owner }, { "itemlabelFilter", itemLabelFilter }, { "top", top }, { "skip", skip } });
+            return JsonConvert.DeserializeObject<JsonCollection<Label>>(response);
+        }
+
+        /// <summary>
+        /// Get label by id
+        /// </summary>
+        /// <param name="labelId"></param>
+        /// <param name="maxItemCount"></param>
+        /// <returns></returns>
+        public async Task<Label> GetLabel(string labelId, int? maxItemCount = null)
+        {
+            string response = await GetResponse(string.Format("labels/{0}", labelId), new Dictionary<string, object>() { { "maxItemCount", maxItemCount } });
+            return JsonConvert.DeserializeObject<Label>(response);
+        }
+
+        /// <summary>
+        /// Get list of labelled items
+        /// </summary>
+        /// <param name="labelId"></param>
+        /// <param name="top"></param>
+        /// <param name="skip"></param>
+        /// <returns></returns>
+        public async Task<JsonCollection<VersionControlItem>> GetLabelledItems(string labelId, int? top = null, int? skip = null)
+        {
+            string response = await GetResponse(string.Format("labels/{0}/items", labelId), new Dictionary<string, object>() { { "top", top }, { "skip", skip } });
+            return JsonConvert.DeserializeObject<JsonCollection<VersionControlItem>>(response);
+        }
+    }
+}
